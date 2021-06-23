ac95058e4487c6a349d47572517ec375503d9e5d
luke-ram <sooramho@gmail.com>
2021-06-23 09:41:28 +0900

    url pattern matching

--- src/main/java/io/security/corespringsecurity/security/metadatasource/UrlFilterInvocationSecurityMetaDataSource.java
+++ src/main/java/io/security/corespringsecurity/security/metadatasource/UrlFilterInvocationSecurityMetaDataSource.java
@@ -10,36 +10,36 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
-public class UrlFilterInvocationSecurityMetadatsSource implements FilterInvocationSecurityMetadataSource {
+public class UrlFilterInvocationSecurityMetaDataSource implements FilterInvocationSecurityMetadataSource {
 
     private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> requestMap = new LinkedHashMap<>();
 
     @Override
     public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
 
-        HttpServletRequest request = ((FilterInvocation) object).getRequest();
-
         requestMap.put(new AntPathRequestMatcher("/mypage"), Arrays.asList(new SecurityConfig("ROLE_USER")));
 
-        if(requestMap != null){
-            for(Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap.entrySet()){
+        HttpServletRequest request = ((FilterInvocation) object).getRequest();
+        if (requestMap != null) {
+            for (Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap.entrySet()) {
                 RequestMatcher matcher = entry.getKey();
-                if(matcher.matches(request)){
+                if (matcher.matches(request)) {
                     return entry.getValue();
                 }
             }
-        }
 
+        }
         return null;
     }
 
     @Override
     public Collection<ConfigAttribute> getAllConfigAttributes() {
-        Set<ConfigAttribute> allAttributes = new HashSet<>();
+        Set<ConfigAttribute> allAttributes = new HashSet();
+        Iterator var2 = this.requestMap.entrySet().iterator();
 
-        for (Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap
-                .entrySet()) {
-            allAttributes.addAll(entry.getValue());
+        while (var2.hasNext()) {
+            Map.Entry<RequestMatcher, List<ConfigAttribute>> entry = (Map.Entry) var2.next();
+            allAttributes.addAll((Collection) entry.getValue());
         }
 
         return allAttributes;
